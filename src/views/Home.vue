391bc1c90fc8e181a1c2c36e660bec4257c505af
Sundze <keyonmwesundze@mail.com>
2023-07-24 16:10:43 +0100

    axios and api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,34 @@
 <template>
-  <pre> {{ meals }} </pre>
+  <div class="flex flex-col p-8">
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search for Meals"
+    />
+    <div class="flex gap-2 mt-2 justify-center">
+      <router-link
+        :to="{ name: 'byLetter', params: { letter } }"
+        v-for="letter of letters"
+        :key="letter"
+      >
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
-import store from "../store";
+import { onMounted, ref } from "vue";
+//import store from "../store";
+import axiosClient from "../axiosClient";
 
-const meals = computed(() => store.state.meals);
+//const meals = computed(() => store.state.meals);
+const letters = "ABCDEFGHIJKLM".split("");
+const ingredients = ref([]);
+
+onMounted(async () => {
+  const response = await axiosClient.get("/list.php?i=list");
+  console.log(response.data);
+  ingredients.value = response.data;
+});
 </script>
