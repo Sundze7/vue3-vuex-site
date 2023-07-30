159badbe98d21b49b0ea94cfe814fb65e3564332
Sundze <keyonmwesundze@mail.com>
2023-07-30 07:40:20 +0100

    mealsDetails page

--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -3,10 +3,28 @@
     <pre>{{ meal }}</pre>
     <h1 class="text-5xl font-bold mb-5">{{ meal.strMeal }}</h1>
     <img :src="meal.strMealThumb" :alt="meal.strMeal" />
-    <div class="grid grid-cols-1 md:grid-cols-3">
+    <div class="grid grid-cols-1 md:grid-cols-3 text-lg py-2">
       <div>
         <strong class="font-bold">Category:</strong> {{ meal.strCategory }}
       </div>
+      <div><strong class="font-bold">Area:</strong> {{ meal.strArea }}</div>
+      <div><strong class="font-bold">Tags:</strong> {{ meal.strTags }}</div>
+    </div>
+
+    <div class="grid grid-cols-1 md:grid-cols-2">
+      <div>
+        <h2 class="text-2xl font-semibold mb-2">Ingredients</h2>
+        <ul>
+          <template v-for="(el, ind) of new Array(20)">
+            <li v-if="meal[`strIngredient${ind + 1}`]" :key="ind">
+              {{ ind + 1 }}. {{ meal[`strIngredient${ind + 1}`] }}
+            </li>
+          </template>
+        </ul>
+      </div>
+    </div>
+    <div>
+      <h2 class="text-2xl font-semibold mb-2">Measures</h2>
     </div>
   </div>
 </template>
