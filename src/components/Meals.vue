60bf45d0a6da1093dab409ada50df92dd5a824c4
Sundze <keyonmwesundze@mail.com>
2023-08-17 05:12:11 +0100

    ..updates

--- src/components/Meals.vue
+++ src/components/Meals.vue
@@ -4,8 +4,17 @@
   >
     <meal-item v-for="meal of meals" :key="meal.idMeal" :meal="meal" />
   </div>
-  <div v-if="!meals.length" class="flex justify-center text-gray-600">
-    There are no meals
+  <div v-if="!meals.length" class="flex justify-center">
+    <div class="bg-yellow-100 bg-opacity-75 p-5 rounded-xl">
+      <h2 class="text-4xl font-semibold mb-2 text-orange-600 text-center">
+        Alert Alert !!!
+      </h2>
+      <h2 class="text-4xl font-semibold mb-6 text-orange-600 text-center">
+        (No Meals)
+      </h2>
+      <img src="../assets/img/go.gif" alt="no meals" />
+      <p class="text-2xl">Perform a search to obtain meals</p>
+    </div>
   </div>
 </template>
 
